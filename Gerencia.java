cf74abd9cc3a8e785c0b0a60f4df325a58171930
MacriFabiane <mariacristinafabiane@gmail.com>
2025-02-07 10:11:40 -0300

    att

--- Gerencia.java
+++ Gerencia.java
@@ -1,3 +1,39 @@
-public class Gerencia {
+import java.util.Scanner;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class Gerencia implements Serializable {
+
+    private LocalDate hoje = LocalDate.now();
+    private int mesAtual = hoje.getMonthValue();
+    private Motorista[] motorista;
+    private int n = 20; //suporta só até 20 motoristas
+    private float somaSalarios = 0;
+
+    //Construtor
+    public Gerencia() {
+        this.n = 20;
+        this.motoristas = new Motrista[n];
+
+        for (int i=0; i<motoristas.lenth; i++){
+            motoristas[i] = null; //define cada elemento como nulo
+        }
+    }
+
+    public registrarMotorista(nome, CNH, dataAdm, dataDem, placa, comissao, salario, senha){
+
+    }
+
+    public gerarListaMotoristaSalario(){
+
+    }
+
+    public calcularTotalSalarios(){
+        
+        for (int i=0; i<motoristas.lenth; i++){
+           somaSalarios = motoristas[i].salario;
+        }
+
+    }
     
 }
