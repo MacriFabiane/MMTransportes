80645f82e13013355d41ecf2c42af5ad96ba67b4
mariaisabelasn <mariaisabelasn@gmail.com>
2025-02-07 20:49:42 -0300

    att

--- Gerencia.java
+++ Gerencia.java
@@ -8,6 +8,7 @@ public class Gerencia implements Serializable {
     private LocalDate hoje = LocalDate.now();
     private int mesAtual = hoje.getMonthValue();
     private Motorista[] motoristas;
+    private Caminhao[] caminhoes;
     private int n = 20; //suporta só até 20 motoristas
     private double somaSalarios = 0;
 
@@ -15,15 +16,19 @@ public class Gerencia implements Serializable {
     public Gerencia() {
         this.n = 20;
         this.motoristas = new Motorista[n];
-        //fazer um vetor de caminhão
+        this.caminhoes = new Caminhao[n];
 
         for (int i=0; i<motoristas.length; i++){
             motoristas[i] = null; //define cada elemento como nulo
+        }
+        for (int i=0; i<caminhoes.length; i++){
+            caminhoes[i] = null; //define cada elemento como nulo
         }
     }
 
     public void registrarMotorista(){ //exemplo em campeonato
         //logica para checkar o caminhão q não tá em uso e definir a placa
+        
     }
 
     public void registrarcaminhao(){
