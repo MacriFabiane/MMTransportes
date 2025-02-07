36e35ba5a8f86b3888ef23cd3f66c05bb8f63d41
MacriFabiane <mariacristinafabiane@gmail.com>
2025-02-07 01:02:47 -0300

    deletando funções antigas

--- Caminhao.java
+++ Caminhao.java
@@ -4,7 +4,6 @@ public class Caminhao implements Serializable{
     private String chassi = new String();
     private boolean emUso;
    
-
     // Construtor
     public Caminhao(String placa, String chassi, Boolean emUso) {
         this.placa = placa;
@@ -27,32 +26,4 @@ public class Caminhao implements Serializable{
     public void setEmUso(boolean x) {//vai colocar a pontuação das jogadas
         emUso = x;
     }
-
-    public int getJogoGeneral(int i) { // Função para pegar as jogadas da "ficha" dos respectivos jogadores.
-        return this.jogoGeneral.getJogadas(i);
-    }
-
-    public JogoGeneral getJogo() { //para acessar por outra classe o jogo de cada jogador
-        return this.jogoGeneral;
-    }
-
-    public void dell(){ //deleta os dados do jogador
-        this.nome=null;
-        this.tipoJogador=null;
-        this.jogoGeneral=null;
-    }
-
-    public void mostrarJogadasExecutadas() {
-        // Jogadas já feitas
-		for (int i = 0 ; i < 13 ; i++) { 
-			if(this.jogoGeneral.getJogadas(i) !=-1) {
-				System.out.printf("%d\t", this.jogoGeneral.getJogadas(i)); 
-			} 
-            else if(this.jogoGeneral.getJogadas(i) ==-1) {
-				System.out.print("-\t");
-			}
-		}
-		System.out.println("");
-    }
-
 }
